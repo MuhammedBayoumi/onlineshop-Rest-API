f8e802b5dc309d5bc70a71144a50c458bc92b530
Muhammed Bayoumi <88550137+MuhammedBayoumi@users.noreply.github.com>
2025-04-29 17:40:04 -0700

    Add files via upload

---
+++ onlineshop-Rest-API/src/main/java/onlineshop/rest/api/OnlineshopRestApiApplication.java
@@ -0,0 +1,13 @@
+package onlineshop.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshopRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineshopRestApiApplication.class, args);
+	}
+
+}
---
+++ onlineshop-Rest-API/src/test/java/onlineshop/rest/api/OnlineshopRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package onlineshop.rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshopRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
